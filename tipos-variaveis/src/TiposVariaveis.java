fafb04ecd77256cbf2600e5ae33e232efeb3205e
ThiagoChz <tyagosouza091@gmail.com>
2024-02-14 13:30:17 -0300

    V1.0

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,13 +1,34 @@
-import java.sql.Date;
 
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        String nome = "Thiago";
+     /*    String nome = "Thiago";
         int idade = 22;
         double peso = 68.5;
         char sexo = 'M';
         boolean doadorOrgao = false;
-        Date dataNascimento = new Date();
+        Date dataNascimento = new Date(2023);
+      */
+
+     /*  for (int i = 0; i < 10; i++){
+            System.out.println(i);
+      }
+      */
+
+      /*int a, b;
+
+      a = 6;
+      b = 6;
+
+     String resultado =  a==b ? "true" : "false";
+
+     System.out.println(resultado);
+     */
+     
+     String nomeUm = "t";
+     String nomeDois = new String("t");
+
+     System.out.println(nomeUm.equals(nomeDois));
+
 
     }
 }
