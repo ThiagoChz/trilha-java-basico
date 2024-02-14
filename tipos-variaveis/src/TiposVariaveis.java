08238c19d21eab5677441b6bfbc9434f57be1f4f
ThiagoChz <tyagosouza091@gmail.com>
2024-02-14 08:57:40 -0300

    v1.0

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,13 @@
+import java.sql.Date;
+
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String nome = "Thiago";
+        int idade = 22;
+        double peso = 68.5;
+        char sexo = 'M';
+        boolean doadorOrgao = false;
+        Date dataNascimento = new Date();
+
     }
 }
